7cfbc8ae88abc778f33f0ec665926c177e7dbd7b
Jagan Elavarasan <jaganelavarasan@gmail.com>
2023-11-02 15:17:01 +0530

    fix(cdk): fix OS deps

--- install.sh
+++ install.sh
@@ -6,21 +6,23 @@ fi
 # Install dependencies
 curl -fsSL https://bun.sh/install | bash
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
-sudo mv kubectl /usr/local/bin/
-kubectl version --client
 npm install -g aws-cdk
 cdk --version
+os=$(uname)
+if [ "$os" == "Linux" ]; then
+  ./linux_deps.sh
+elif [ "$os" == "Darwin" ]; then
+  ./mac_deps.sh
+else
+  echo "Unsupported operating system."
+  exit 1
+fi
 
 # Read the DB Password and Admin API Key
 echo "Enter DB Password (Min 8 Character Needed [A-Z][a-z][0-9]): "  
 read -s DB_PASS
 echo "Enter Admin API Key: "  
 read -s ADMIN_API_KEY
-# Replace the DB Password and Admin API Key in the index.ts file
-awk -v old="dbpassword" -v new="$DB_PASS" '{gsub(old, new); print}' index.ts > index_new.ts && mv index_new.ts index.ts
-awk -v old="test_admin" -v new="$ADMIN_API_KEY" '{gsub(old, new); print}' index.ts > index_new.ts && mv index_new.ts index.ts
 
 # Deploy the EKS Cluster
 bun install
@@ -44,6 +46,7 @@ LB_SG=$(aws cloudformation describe-stacks --stack-name hyperswitch --query "Sta
 kubectl delete deployment hyperswitch-consumer-consumer-v1o47o0ohotfixo3 -n hyperswitch
 kubectl delete deployment hyperswitch-server-v1o52o1v2 -n hyperswitch 
 # Deploy the hyperswitch application with the load balancer host name
+helm repo add hs https://juspay.github.io/hyperswitch-helm
 helm upgrade --install hypers-v1 hs/hyperswitch-helm --set "application.server.server_base_url=http://$APP_HOST,db.host=$DB_HOST,db.password=$DB_PASS,redis.host=$REDIS_HOST,loadBalancer.targetSecurityGroup=$LB_SG" -n hyperswitch 
 sleep 30
 echo "App server running on "$APP_HOST
