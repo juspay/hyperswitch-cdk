8a006fc99218b689f0352bae79c0d80e497f56a0
Venkatesh <inventvenkat@gmail.com>
2023-11-06 14:35:45 +0530

    refactor: refactor configuration usage in awsStack (#16)
    Co-authored-by: venkatesh.devendran <venkatesh.devendran@juspay.in>

--- lib/aws/stack.ts
+++ lib/aws/stack.ts
@@ -17,30 +17,19 @@ export class AWSStack extends cdk.Stack {
     if(is_root_user)
       throw new Error("Please create new user with appropiate role as ROOT user is not recommended");
     super(scope, config.stack.name, {stackName: config.stack.name});
+
     let vpc = new Vpc(this, config.vpc);
     let subnets = new SubnetStack(this, vpc.vpc, config);
     let elasticache = new ElasticacheStack(this, config, vpc.vpc);
-    let db_pass = scope.node.tryGetContext('db_pass') || "dbpassword";
-    let admin_api_key = scope.node.tryGetContext('admin_api_key') || "test_admin"
-    let isStandalone = scope.node.tryGetContext('test') || false;
-    let rds = new DataBaseConstruct( this, // create database master user secret and store it in Secrets Manager
-      {
-        port: 5432,
-        db_user: "db_user",
-        password: db_pass,
-        writer_instance_class: ec2.InstanceClass.T3,
-        writer_instance_size: ec2.InstanceSize.MEDIUM,
-        reader_instance_class: ec2.InstanceClass.T3,
-        reader_instance_size: ec2.InstanceSize.MEDIUM,
-      },
-      vpc.vpc
-    );
+    let rds = new DataBaseConstruct( this, config.rds ,vpc.vpc);
     rds.sg.addIngressRule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(5432)); // this is required to connect db from local
 
+    update_config(config, rds.db_cluster.clusterEndpoint.hostname, elasticache.cluster.attrRedisEndpointAddress)
+
+    let isStandalone = scope.node.tryGetContext('test') || false;
     if (isStandalone){
       console.log("Deploying Standalone")
-      let db_host = rds.db_cluster.clusterEndpoint.hostname;
-      let hyperswitch_ec2 = new EC2Instance(this, vpc.vpc, "hyperswitch", elasticache.cluster.attrRedisEndpointAddress, db_host, db_pass, admin_api_key, "db_user", "hyperswitch");
+      let hyperswitch_ec2 = new EC2Instance(this, vpc.vpc, config);
       rds.sg.addIngressRule(hyperswitch_ec2.sg, ec2.Port.tcp(5432));
       elasticache.sg.addIngressRule(hyperswitch_ec2.sg, ec2.Port.tcp(6379));
       hyperswitch_ec2.sg.addEgressRule(rds.sg, ec2.Port.tcp(5432));
@@ -49,11 +38,15 @@ export class AWSStack extends cdk.Stack {
       hyperswitch_ec2.sg.addIngressRule(ec2.Peer.ipv4('0.0.0.0/0'), ec2.Port.tcp(22));
     }else{
       console.log("Deploying production")
-      let eks = new EksStack(this, config, vpc.vpc, rds, elasticache, admin_api_key);
+      let eks = new EksStack(this, config, vpc.vpc, rds, elasticache, config.hyperswitch_ec2.admin_api_key);
       rds.sg.addIngressRule(eks.sg, ec2.Port.tcp(5432));
       elasticache.sg.addIngressRule(eks.sg, ec2.Port.tcp(6379));
     }
-
-
   }
+}
+
+function update_config(config:Config, db_host:string, redis_host:string){
+  config.hyperswitch_ec2.db_host = db_host;
+  config.hyperswitch_ec2.redis_host = redis_host;
+  return config;
 }
