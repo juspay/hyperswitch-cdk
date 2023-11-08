0f4a209b2e1a79ff928d317690c01f0beaf6648b
BallaNitesh <126162378+BallaNitesh@users.noreply.github.com>
2023-11-08 16:01:32 +0530

    Update eks.ts

--- lib/aws/eks.ts
+++ lib/aws/eks.ts
@@ -225,7 +225,7 @@ export class EksStack {
         release: "loki",
         values: {
           grafana: {
-            version: 10.0.1,
+            version: "10.0.1",
             enabled: true,
             adminPassword: "admin",
             serviceAccount: {
