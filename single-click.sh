07dc9d268c5d12f337396947b009a12db6224912
Nitesh Balla <nitesh.balla@juspay.in>
2023-11-22 20:01:20 +0530

    fix: fix script issues

--- single-click.sh
+++ single-click.sh
@@ -5,7 +5,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install 18 -y
 nvm use 18
-npm install yarn -g
+npm install -g aws-cdk
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
 mv ./kubectl /usr/local/bin/kubectl
@@ -14,7 +14,7 @@ AWS_ARN=$(aws sts get-caller-identity --output json | jq -r .Arn )
 AWS_ACCOUNT=$(aws sts get-caller-identity --output json | jq -r .Account)
 wget https://github.com/juspay/hyperswitch-cdk/archive/refs/heads/main.zip
 unzip main.zip
-cd hyperswitch-cdk-main
-yarn install
-yarn cdk bootstrap aws://$AWS_ACCOUNT/$AWS_REGION -c aws_arn=$AWS_ARN
-yarn cdk deploy --require-approval never -c db_pass=$DB_PASS -c admin_api_key=$ADMIN_API_KEY -c aws_arn=$AWS_ARN
+cd $(unzip -Z -1 main.zip| head -1)
+npm install
+cdk bootstrap aws://$AWS_ACCOUNT/$AWS_REGION -c aws_arn=$AWS_ARN
+cdk deploy --require-approval never -c db_pass=$DB_PASS -c admin_api_key=$ADMIN_API_KEY -c aws_arn=$AWS_ARN
