3461037434e74caa0864d18a45fa2f218e35d86c
Nishant Joshi <nishant.joshi@juspay.in>
2023-11-30 18:22:34 +0530

    chore: fix script echo

--- install-locker.sh
+++ install-locker.sh
@@ -34,7 +34,7 @@ if [[ $AWS_ARN == *":root"* ]]; then
 fi
 echo "##########################################"
 
-echo -e "$(tput bold)$(tput setaf 2)Install Locker Standalone Setup$(tput sgr0)"
+echo "$(tput bold)$(tput setaf 2)Install Locker Standalone Setup$(tput sgr0)"
 
 
 
@@ -68,8 +68,8 @@ if [[ -n "$VPC_ID" ]]; then
 fi
 
 
-echo -e "$(tput bold)$(tput setaf 3)To generated the master key, you can use the utility bundled within \n(https://github.com/juspay/hyperswitch-card-vault)$(tput sgr0)"
-echo -e "$(tput bold)$(tput setaf 3)If you have cargo installed you can run \n(cargo install --git https://github.com/juspay/hyperswitch-card-vault --bin utils --root . && ./bin/utils master-key && rm ./bin/utils && rmdir ./bin)$(tput sgr0)"
+echo "$(tput bold)$(tput setaf 3)To generated the master key, you can use the utility bundled within \n(https://github.com/juspay/hyperswitch-card-vault)$(tput sgr0)"
+echo "$(tput bold)$(tput setaf 3)If you have cargo installed you can run \n(cargo install --git https://github.com/juspay/hyperswitch-card-vault --bin utils --root . && ./bin/utils master-key && rm ./bin/utils && rmdir ./bin)$(tput sgr0)"
 
 read -r -s -p "Enter the generated master key: " MASTER_KEY
 
@@ -79,7 +79,7 @@ read -r -s -p "Enter the database password to be used: " DB_PASS
 
 echo
 
-echo -e "$(tput bold)$(tput setaf 3)Please make sure that the following environment variables are set properly:\n- AWS_DEFAULT_REGION\n- AWS_PROFILE$(tput sgr0)"
+echo "$(tput bold)$(tput setaf 3)Please make sure that the following environment variables are set properly:\n- AWS_DEFAULT_REGION\n- AWS_PROFILE$(tput sgr0)"
 
 if ! ask_yes_no "Continue with the installation?"; then
     exit 1
@@ -101,31 +101,30 @@ cdk bootstrap aws://"$AWS_ACCOUNT"/"$AWS_DEFAULT_REGION" -c aws_arn="$AWS_ARN"
 
 cdk deploy --require-approval never -c master_key="$MASTER_KEY" -c db_pass="$DB_PASS" -c stack="card-vault" -c locker_jump=$JUMP_SERVER $LOCKER_FLAGS  > "$TEMP_FILE"
 
-export JUMP_COMMAND=$(grep ''$STACK'.GetJumpLockerSSHKey' < "$TEMP_FILE" | sed 's/'$STACK'.GetJumpLockerSSHKey = \(.*\)/\1/g')
+export JUMP_COMMAND=$(grep 'GetJumpLockerSSHKey' < "$TEMP_FILE" | sed 's/.*GetJumpLockerSSHKey = \(.*\)/\1/g')
 
-export JUMP_IP=$(grep ''$STACK'.JumpLockerPublicIP' < "$TEMP_FILE" | sed 's/'$STACK'.JumpLockerPublicIP = \(.*\)/\1/g')
+export JUMP_IP=$(grep 'JumpLockerPublicIP' < "$TEMP_FILE" | sed 's/.*JumpLockerPublicIP = \(.*\)/\1/g')
 
-export LOCKER_COMMAND=$(grep ''$STACK'.GetLockerSSHKey' < "$TEMP_FILE" | sed 's/'$STACK'.GetLockerSSHKey = \(.*\)/\1/g')
+export LOCKER_COMMAND=$(grep 'GetLockerSSHKey' < "$TEMP_FILE" | sed 's/.*GetLockerSSHKey.* = \(.*\)/\1/g')
 
-export LOCKER_IP=$(grep ''$STACK'.Lockerec2IP' < "$TEMP_FILE"| sed 's/'$STACK'.Lockerec2IP = \(.*\)/\1/g')
+export LOCKER_IP=$(grep 'LockerIP' < "$TEMP_FILE"| sed 's/.*LockerIP.* = \(.*\)/\1/g')
 
 
 echo "
 $JUMP_COMMAND
-chmod 400 locker-jump.pem
 $LOCKER_COMMAND
+chmod 400 locker-jump.pem
 
-scp -i locker-jump.pem ./locker.pem ec2-user@$JUMP_IP:/home/ec2-user/locker.pem
-ssh -i locker-jump.pem ec2-user@JUMP_IP 'bash -c \"echo export LOCKER_IP=$LOCKER_IP >> /home/ec2-user/.bashrc && chmod 400 /home/ec2-user/locker.pem\"'
 ssh -i locker-jump.pem ec2-user@$JUMP_IP
 
-ssh -i locker.pem ec2-user@\$LOCKER_IP
 
 unset HISTFILE
 
-curl -X 'POST' 'localhost:8080/custodian/key1' -H 'Content-Type: application/json' --data '{ \"key\": \"<ENTER_YOUR_KEY1_HERE>\" }'
-curl -X 'POST' 'localhost:8080/custodian/key2' -H 'Content-Type: application/json' --data '{ \"key\": \"<ENTER_YOUR_KEY2_HERE>\" }'
-curl -X 'POST' 'localhost:8080/custodian/decrypt'
+curl -X 'POST' '$LOCKER_IP:8080/custodian/key1' -H 'Content-Type: application/json' --data '{ \"key\": \"<ENTER_YOUR_KEY1_HERE>\" }'
+curl -X 'POST' '$LOCKER_IP:8080/custodian/key2' -H 'Content-Type: application/json' --data '{ \"key\": \"<ENTER_YOUR_KEY2_HERE>\" }'
+curl -X 'POST' '$LOCKER_IP:8080/custodian/decrypt'
+
+curl -X 'POST' '$LOCKER_IP:8080/health'
 "
 
 echo
