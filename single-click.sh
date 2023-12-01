ea53db242eeba4246eaa109f1094b41238552d8d
Jagan <jaganelavarasan@gmail.com>
2023-12-01 20:26:16 +0530

    fix(cdk): fix bugs in one-click installation and add locker support (#45)

--- single-click.sh
+++ single-click.sh
@@ -19,7 +19,12 @@ unzip main.zip
 cd $(unzip -Z -1 main.zip| head -1)
 npm install
 cdk bootstrap aws://$AWS_ACCOUNT/$AWS_REGION -c aws_arn=$AWS_ARN
-cdk deploy --require-approval never -c db_pass=$DB_PASS -c admin_api_key=$ADMIN_API_KEY -c aws_arn=$AWS_ARN
+LOCKER=""
+if [[ -n "$MASTER_KEY" ]]; then
+  LOCKER+="-c master_key=$MASTER_KEY "
+  LOCKER+="-c locker_pass=$LOCKER_DB_PASS "
+fi
+cdk deploy --require-approval never -c db_pass=$DB_PASS -c admin_api_key=$ADMIN_API_KEY -c aws_arn=$AWS_ARN $LOCKER
 aws eks update-kubeconfig --region $AWS_REGION --name hs-eks-cluster
 export KUBECONFIG=~/.kube/config
 sleep 10
@@ -52,18 +57,27 @@ export CONNECTOR_KEY=$(curl --silent --location --request POST 'http://'$APP_HOS
 --header 'Accept: application/json' \
 --header 'api-key: '$ADMIN_API_KEY \
 --data-raw '{"connector_type":"fiz_operations","connector_name":"stripe_test","connector_account_details":{"auth_type":"HeaderKey","api_key":"test_key"},"test_mode":true,"disabled":false,"payment_methods_enabled":[{"payment_method":"card","payment_method_types":[{"payment_method_type":"credit","card_networks":["Visa","Mastercard"],"minimum_amount":1,"maximum_amount":68607706,"recurring_enabled":true,"installment_payment_enabled":true},{"payment_method_type":"debit","card_networks":["Visa","Mastercard"],"minimum_amount":1,"maximum_amount":68607706,"recurring_enabled":true,"installment_payment_enabled":true}]},{"payment_method":"pay_later","payment_method_types":[{"payment_method_type":"klarna","payment_experience":"redirect_to_url","minimum_amount":1,"maximum_amount":68607706,"recurring_enabled":true,"installment_payment_enabled":true},{"payment_method_type":"affirm","payment_experience":"redirect_to_url","minimum_amount":1,"maximum_amount":68607706,"recurring_enabled":true,"installment_payment_enabled":true},{"payment_method_type":"afterpay_clearpay","payment_experience":"redirect_to_url","minimum_amount":1,"maximum_amount":68607706,"recurring_enabled":true,"installment_payment_enabled":true}]}],"metadata":{"city":"NY","unit":"245"},"connector_webhook_details":{"merchant_secret":"MyWebhookSecret"}}' )
-helm upgrade --install hypers-v1 hs/hyperswitch-helm --set "application.dashboard.env.apiBaseUrl=http://$APP_HOST,application.sdk.env.hyperswitchPublishableKey=$PUB_KEY,application.sdk.env.hyperswitchSecretKey=$API_KEY,application.sdk.env.hyperswitchServerUrl=http://$APP_HOST,application.sdk.env.hyperSwitchClientUrl=$SDK_URL,application.sdk.image=$SDK_IMAGE,application.dashboard.env.sdkBaseUrl=$SDK_URL/HyperLoader.js,application.server.image=juspaydotin/hyperswitch-router:v1.78.0-standalone,application.server.server_base_url=http://$APP_HOST,application.server.secrets.admin_api_key=$ADMIN_API_KEY,db.host=$DB_HOST,db.password=$DB_PASS,redis.host=$REDIS_HOST,loadBalancer.targetSecurityGroup=$LB_SG" -n hyperswitch
-export body="App Host: $APP_HOST\nLogs Host: $LOGS_HOST\nControl Center Host: $CONTROL_CENTER_HOST\nSDK Host: $SDK_HOST\nRedis Host: $REDIS_HOST\nDB Host: $DB_HOST\nSDK URL: $SDK_URL\nSDK Image: $SDK_IMAGE\nMerchant ID: $MERCHANT_ID\nPublishable Key: $PUB_KEY\nAPI Key: $API_KEY\nConnector Key: $CONNECTOR_KEY"
-echo "$body"
-# Subscribe to the email-one-click API
-curl -X POST -H "Content-Type: application/json" -d "{\"recipient\": \"$MAILID\"}" https://8x2foxz2bd.execute-api.us-east-1.amazonaws.com/default/email-one-click
-
-payload=$(jq -n \
-  --arg recipient "$MAILID" \
-  --arg name "$NAME" \
-  --arg body "$body" \
-  '{recipient: $recipient, name: $name, body: $body}')
-
-curl -X POST -H "Content-Type: application/json" \
--d "$payload" \
-https://8x2foxz2bd.execute-api.us-east-1.amazonaws.com/default/email-one-click
+helm get values -n hyperswitch hypers-v1 > values.yaml
+helm upgrade --install hypers-v1 hs/hyperswitch-helm --set "application.dashboard.env.apiBaseUrl=http://$APP_HOST,application.sdk.env.hyperswitchPublishableKey=$PUB_KEY,application.sdk.env.hyperswitchSecretKey=$API_KEY,application.sdk.env.hyperswitchServerUrl=http://$APP_HOST,application.sdk.env.hyperSwitchClientUrl=$SDK_URL,application.dashboard.env.sdkBaseUrl=$SDK_URL/HyperLoader.js,application.server.server_base_url=http://$APP_HOST" -n hyperswitch -f values.yaml
+sleep 240
+export BOLD=$(tput bold)
+export BLUE=$(tput setaf 4)
+export GREEN=$(tput setaf 2)
+export YELLOW=$(tput setaf 3)
+export RESET=$(tput sgr0)
+export LOG_FILE="cdk.services.log"
+function echoLog() {
+  echo "$1" | tee -a $LOG_FILE
+}
+echoLog "--------------------------------------------------------------------------------"
+echoLog "$BOLD Service                           Host$RESET"
+echoLog "--------------------------------------------------------------------------------"
+echoLog "$GREEN HyperloaderJS Hosted at           $BLUE"$SDK_URL/HyperLoader.js"$RESET"
+echoLog "$GREEN App server running on             $BLUE"http://$APP_HOST"$RESET"
+echoLog "$GREEN Logs server running on            $BLUE"http://$LOGS_HOST"$RESET, Login with $YELLOW username: admin, password: admin$RESET , Please change on startup"
+echoLog "$GREEN Control center server running on  $BLUE"http://$CONTROL_CENTER_HOST"$RESET, Login with $YELLOW Email: test@gmail.com, password: admin$RESET , Please change on startup"
+echoLog "$GREEN Hyperswitch Demo Store running on $BLUE"http://$SDK_HOST"$RESET"
+echoLog "--------------------------------------------------------------------------------"
+echoLog "##########################################"
+aws s3 cp cdk.services.log s3://hyperswitch-schema-$AWS_ACCOUNT-$AWS_REGION/cdk.services.log
+echo "$BLUE Please run 'cat cdk.services.log' to view the services details again"$RESET
