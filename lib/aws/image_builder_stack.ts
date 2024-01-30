33720945b14137043ae6cbcb57a59d5c7409ebfa
dracarys18 <karthikey.hegde@juspay.in>
2024-01-30 13:17:21 +0530

    fix: add function for repeated code

--- lib/aws/image_builder_stack.ts
+++ lib/aws/image_builder_stack.ts
@@ -10,13 +10,60 @@ import { Bucket } from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { readFileSync } from "fs";
 
+type ImageBuilderProperties = {
+    pipeline_name: string;
+    recipe_name: string;
+    profile_name: string;
+    comp_name: string;
+    comp_id: string;
+    infra_config_name: string;
+    baseimageArn: string;
+    description: string;
+    compFilePath: string;
+}
+
+function CreateImagePipeline(
+    stack: ImageBuilderStack,
+    role: iam.Role,
+    props: ImageBuilderProperties
+) {
+    const component = new image_builder.CfnComponent(stack, props.comp_id, {
+        name: props.comp_name,
+        description: props.description,
+        platform: "Linux",
+        version: "1.0.1",
+        data: readFileSync(props.compFilePath).toString(),
+    })
+    let instance_profile = new iam.CfnInstanceProfile(stack, props.profile_name, { instanceProfileName: props.profile_name, roles: [role.roleName] })
+
+    const squid_recipe = new image_builder.CfnImageRecipe(stack, props.recipe_name, {
+        name: props.recipe_name,
+        version: "1.0.3",
+        components: [
+            { "componentArn": component.attrArn }
+        ],
+        parentImage: props.baseimageArn,
+    })
+
+    let squid_infra_config = new image_builder.CfnInfrastructureConfiguration(stack, props.infra_config_name, {
+        name: props.infra_config_name,
+        instanceTypes: ["t3.medium"],
+        instanceProfileName: props.profile_name
+    })
+    squid_infra_config.addDependency(instance_profile)
+
+    let pipeline = new image_builder.CfnImagePipeline(stack, props.pipeline_name, {
+        name: props.pipeline_name,
+        imageRecipeArn: squid_recipe.attrArn,
+        infrastructureConfigurationArn: squid_infra_config.attrArn
+    })
+
+    pipeline.addDependency(squid_infra_config)
+}
+
 export class ImageBuilderStack extends cdk.Stack {
     constructor(scope: Construct, config: ImageBuilderConfig) {
         super(scope, config.name, {
-            // env: {
-            //   account: process.env.CDK_DEFAULT_ACCOUNT,
-            //   region: process.env.CDK_DEFAULT_REGION
-            // },
             stackName: config.name,
         });
 
@@ -33,72 +80,40 @@ export class ImageBuilderStack extends cdk.Stack {
 
         const base_image_arn = MachineImage.latestAmazonLinux2().getImage(this).imageId;
 
-        const component = new image_builder.CfnComponent(this, "install_squid_component", {
-            name: "HyperswitchSquidImageBuilder",
+        let squid_properties = {
+            pipeline_name: "SquidImagePipeline",
+            recipe_name: "SquidImageRecipe",
+            profile_name: "SquidStationInstanceProfile",
+            comp_name: "HyperswitchSquidImageBuilder",
+            comp_id: "install_squid_component",
+            infra_config_name: "SquidInfraConfig",
+            baseimageArn: base_image_arn,
             description: "Image builder for squid",
-            platform: "Linux",
-            version: "1.0.1",
-            data: readFileSync("./components/squid.yml").toString(),
-        })
-        let instance_profile = new iam.CfnInstanceProfile(this, "SquidStationProfile", { instanceProfileName: "SquidStationInstanceProfile", roles: ["StationRole"] })
+            compFilePath: "./components/squid.yml",
+        };
 
-        const squid_recipe = new image_builder.CfnImageRecipe(this, "SquidImageRecipe", {
-            name: "SquidImageRecipe",
-            version: "1.0.3",
-            components: [
-                { "componentArn": component.attrArn }
-            ],
-            parentImage: base_image_arn
-        })
+        CreateImagePipeline(
+            this,
+            role,
+            squid_properties
+        )
 
-        let squid_infra_config = new image_builder.CfnInfrastructureConfiguration(this, "SquidInfraConfig", {
-            name: "SquidInfraConfig",
-            instanceTypes: ["t3.medium"],
-            instanceProfileName: "SquidStationInstanceProfile"
-        })
-        squid_infra_config.addDependency(instance_profile)
-
-        let pipeline = new image_builder.CfnImagePipeline(this, "SquidImagePipeline", {
-            name: "SquidImagePipeLine",
-            imageRecipeArn: squid_recipe.attrArn,
-            infrastructureConfigurationArn: squid_infra_config.attrArn
-        })
-
-        pipeline.addDependency(squid_infra_config)
-
-        const envoy_component = new image_builder.CfnComponent(this, "install_envoy_component", {
-            name: "HyperswitchEnvoyImageBuilder",
+        let envoy_properties = {
+            pipeline_name: "EnvoyImagePipeline",
+            recipe_name: "EnvoyImageRecipe",
+            profile_name: "EnvoyStationProfile",
+            comp_name: "HyperswitchEnvoyImageBuilder",
+            comp_id: "install_envoy_component",
+            infra_config_name: "EnvoyInfraConfig",
+            baseimageArn: base_image_arn,
             description: "Image builder for Envoy",
-            platform: "Linux",
-            version: "1.0.1",
-            data: readFileSync("./components/envoy.yml").toString(),
-        })
+            compFilePath: "./components/envoy.yml",
+        };
 
-        const envoy_recipe = new image_builder.CfnImageRecipe(this, "EnvoyImageRecipe", {
-            name: "EnvoyImageRecipe",
-            version: "1.0.3",
-            components: [
-                { "componentArn": envoy_component.attrArn }
-            ],
-            parentImage: base_image_arn
-        })
-
-        let envoy_instance_profile = new iam.CfnInstanceProfile(this, "EnvoyStationProfile", { instanceProfileName: "EnvoyStationInstanceProfile", roles: ["StationRole"] })
-
-        let envoy_infra_config = new image_builder.CfnInfrastructureConfiguration(this, "EnvoyInfraConfig", {
-            name: "EnvoyInfraConfig",
-            instanceTypes: ["t3.medium"],
-            instanceProfileName: "EnvoyStationInstanceProfile"
-        })
-        envoy_infra_config.addDependency(envoy_instance_profile)
-
-        let envoy_pipeline = new image_builder.CfnImagePipeline(this, "EnvoyImagePipeline", {
-            name: "EnvoyImagePipeLine",
-            imageRecipeArn: envoy_recipe.attrArn,
-            infrastructureConfigurationArn: envoy_infra_config.attrArn
-        })
-
-        envoy_pipeline.addDependency(envoy_infra_config)
-
+        CreateImagePipeline(
+            this,
+            role,
+            envoy_properties
+        )
     }
 }
