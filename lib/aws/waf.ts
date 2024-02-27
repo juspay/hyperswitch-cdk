28bc874e86a575837798640f4d9a009086ef9813
Nishant Joshi <nishant.joshi@juspay.in>
2024-02-27 15:24:02 +0530

    feat(waf): add support for WAF and attach it to kubernetes Ingress

---
+++ lib/aws/waf.ts
@@ -0,0 +1,42 @@
+import { Construct } from "constructs";
+import { aws_wafv2 as wafv2 } from "aws-cdk-lib";
+
+export class WAF extends Construct {
+  readonly waf_arn: string;
+  readonly waf_acl_id: string;
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const webAcl = new wafv2.CfnWebACL(this, "WebACL", {
+      scope: "REGIONAL",
+      defaultAction: {
+        allow: {},
+      },
+      visibilityConfig: {
+        cloudWatchMetricsEnabled: true,
+        metricName: "WebACL",
+        sampledRequestsEnabled: true,
+      },
+      rules: [
+        {
+          name: "CRSRule",
+          priority: 0,
+          statement: {
+            managedRuleGroupStatement: {
+              name: "AWSManagedRulesCommonRuleSet",
+              vendorName: "AWS",
+            },
+          },
+          visibilityConfig: {
+            cloudWatchMetricsEnabled: true,
+            metricName: "CRSRule",
+            sampledRequestsEnabled: true,
+          },
+        },
+      ],
+    });
+
+    this.waf_arn = webAcl.attrArn;
+    this.waf_acl_id = webAcl.attrId;
+  }
+}
