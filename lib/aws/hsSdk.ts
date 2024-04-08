14686fe01474303fb5ddfb54762f7a854f8711aa
Nitesh Balla <nitesh.balla@juspay.in>
2024-04-08 10:28:37 +0530

    feat: add sdk stack file back

---
+++ lib/aws/hsSdk.ts
@@ -0,0 +1,106 @@
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { Construct } from "constructs";
+import * as codebuild from "aws-cdk-lib/aws-codebuild";
+import { Config } from "./config";
+import { DataBaseConstruct } from "./rds";
+import * as cr from "aws-cdk-lib/custom-resources";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import * as cdk from "aws-cdk-lib/core";
+import { EksStack } from "./eks";
+
+export class HyperswitchSDKStack {
+  constructor(
+    scope: Construct,
+    config: Config,
+    vpc: ec2.Vpc,
+    rds: DataBaseConstruct,
+    eks: EksStack
+  ) {
+    // Create a new S3 bucket
+    const bucket = rds.bucket;
+    let sdkVersion = '0.5.6';
+
+    new cdk.CfnOutput(scope, 'HyperLoaderUrl', {
+      value: "http://"+rds.bucket.bucketDomainName+"/"+sdkVersion+"/v0",
+    });
+    new cdk.CfnOutput(scope, 'Hyperswitch Services', {
+      value: "https://"+rds.bucket.bucketDomainName+"/cdk.services.html",
+    });
+
+    // Create a new CodeBuild project
+    const project = new codebuild.Project(scope, "HyperswitchSDK", {
+      projectName: "HyperswitchSDK",
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: "0.2",
+        phases: {
+          install: {
+            commands: [
+              "export envBackendUrl=\"http://$(aws elbv2 describe-load-balancers --name 'hyperswitch' --query 'LoadBalancers[].DNSName' --output text)\"",
+              "git clone --branch v"+sdkVersion+" https://github.com/juspay/hyperswitch-web",
+              "cd hyperswitch-web",
+              "n install 18",
+              "npm -v",
+              "node --version",
+              "npm install",
+              "npm run re:build",
+            ],
+          },
+          build: {
+            commands: "envSdkUrl=$envSdkUrl envBackendUrl=$envBackendUrl npm run build:integ",
+          },
+          post_build: {
+            commands: [
+              "aws s3 cp --recursive dist/integ/ s3://$sdkBucket/"+sdkVersion+"/v0",
+            ],
+          },
+        },
+      }),
+      environmentVariables: {
+        sdkBucket: {
+          value: rds.bucket.bucketName,
+          type: codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+        },
+        envSdkUrl: {
+          value: "http://" + rds.bucket.bucketDomainName,
+          type: codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+        },
+      },
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_5,
+      },
+    });
+    project.node.addDependency(eks.lokiChart);
+
+    project.addToRolePolicy(
+      new PolicyStatement({
+        actions: ["elasticloadbalancing:DescribeLoadBalancers"],
+        resources: ["*"], // Modify this to restrict access to specific resources
+      })
+    );
+
+    project.addToRolePolicy(
+      new PolicyStatement({
+        actions: ["logs:CreateLogStream"],
+        resources: ["*"],
+      })
+    );
+
+    // Allow the CodeBuild project to access the S3 bucket
+    bucket.grantReadWrite(project);
+
+    // Create a custom resource that starts a build when the project is created
+    new cr.AwsCustomResource(scope, "StartBuild", {
+      onCreate: {
+        service: "CodeBuild",
+        action: "startBuild",
+        parameters: {
+          projectName: project.projectName,
+        },
+        physicalResourceId: cr.PhysicalResourceId.of("StartBuild"),
+      },
+      policy: cr.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: cr.AwsCustomResourcePolicy.ANY_RESOURCE,
+      }),
+    });
+  }
+}
