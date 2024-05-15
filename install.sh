3ea5236a794859e64a2ec7faf6b40d69c7f3f13c
Nitesh <126162378+nitesh-balla@users.noreply.github.com>
2024-05-15 15:04:09 +0530

    fix: use appropriate subnet group for migration runner (#142)

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ yellow=$(tput setaf 3)
 red=$(tput setaf 1)
 reset=$(tput sgr0)
 source ./bash/utils.sh
+function echoLog() {
+    echo "$1" | tee -a $LOG_FILE
+}
 
 # Function to display error messages in red
 display_error() {
@@ -452,29 +455,29 @@ else
         aws iam delete-role --role-name $ROLE_NAME 2>/dev/null
         cdk bootstrap aws://$AWS_ACCOUNT_ID/$AWS_DEFAULT_REGION -c aws_arn=$AWS_ARN
     fi
-    if cdk deploy -c aws_arn=$AWS_ARN -c free_tier=true -c db_pass=$DB_PASS -c admin_api_key=$ADMIN_API_KEY; then
+    if cdk deploy --require-approval never -c aws_arn=$AWS_ARN -c free_tier=true -c db_pass=$DB_PASS -c admin_api_key=$ADMIN_API_KEY; then
         STANDALONE_HOST=$(aws cloudformation describe-stacks --stack-name hyperswitch --query "Stacks[0].Outputs[?OutputKey=='StandaloneURL'].OutputValue" --output text)
         CONTROL_CENTER_HOST=$(aws cloudformation describe-stacks --stack-name hyperswitch --query "Stacks[0].Outputs[?OutputKey=='ControlCenterURL'].OutputValue" --output text)
         SDK_HOST=$(aws cloudformation describe-stacks --stack-name hyperswitch --query "Stacks[0].Outputs[?OutputKey=='SdkAssetsURL'].OutputValue" --output text)
         DEMO_APP=$(aws cloudformation describe-stacks --stack-name hyperswitch --query "Stacks[0].Outputs[?OutputKey=='DemoApp'].OutputValue" --output text)
         echo "Please wait for instances to be initialized. Approximate time is 2 minutes."
         printf "${bold}Initializing Instances${reset} "
-        # Start the spinner in the background
-        (
-            while :; do
-                for s in '/' '-' '\\' '|'; do
-                    printf "\r$s"
-                    sleep 1
-                done
-            done
-        ) &
-        spinner_pid=$!
-        # Sleep for 10 seconds to simulate work
+        # # Start the spinner in the background
+        # (
+        #     while :; do
+        #         for s in '/' '-' '\\' '|'; do
+        #             printf "\r$s"
+        #             sleep 1
+        #         done
+        #     done
+        # ) &
+        # spinner_pid=$!
+        # # Sleep for x seconds to simulate work
         sleep 60
         # Kill the spinner
-        kill $spinner_pid >/dev/null 2>&1
-        wait $spinner_pid 2>/dev/null # Ensures the spinner process is properly terminated before moving on
-        printf "\r"                   # Clear the spinner character
+        # kill $spinner_pid >/dev/null 2>&1
+        # wait $spinner_pid 2>/dev/null # Ensures the spinner process is properly terminated before moving on
+        # printf "\r"                   # Clear the spinner character
         printf "\nInitialization complete.\n"
         printf "\n"
         echoLog "--------------------------------------------------------------------------------"
