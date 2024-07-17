1d9172a93e59703f872d3552ee59177e2fda3aec
dracarys18 <karthikey.hegde@juspay.in>
2024-07-17 14:49:33 +0530

    feat: add kms encryption to keymanager

---
+++ lib/aws/keymanager/encryption.py
@@ -0,0 +1,120 @@
+import os
+import json
+import boto3
+import urllib3
+import base64
+
+http = urllib3.PoolManager()
+
+
+def worker():
+    secrets_manager = boto3.client('secretsmanager')
+    ssm_manager = boto3.client('ssm')
+
+    kms_client = boto3.client('kms')
+
+    secret_arn = os.environ['SECRET_MANAGER_ARN']
+    secret_value_response = secrets_manager.get_secret_value(
+        SecretId=secret_arn)
+    credentials = json.loads(secret_value_response['SecretString'])
+
+    kms_fun = kms_encryptor(
+        credentials["kms_id"], credentials["region"], kms_client)
+
+    def enc_pl(x): return kms_fun(credentials[x])
+    def pl(x): return credentials[x]
+
+    db_pass = enc_pl("db_password")
+    ca_cert = enc_pl("ca_cert")
+    tls_key = enc_pl("tls_key")
+    tls_cert = enc_pl("tls_cert")
+
+    secretval = {
+        "db_pass": db_pass,
+        "ca_cert": ca_cert,
+        "tls_key": tls_key,
+        "tls_cert": tls_cert
+    }
+
+    for key in secretval:
+        store_parameter(ssm_manager, key, secretval[key])
+
+
+def kms_encryptor(key_id: str, region: str, kms_client):
+    return lambda data: base64.b64encode(kms_client.encrypt(KeyId=key_id, Plaintext=data)["CiphertextBlob"]).decode("utf-8")
+
+
+def send(event, context, responseStatus, responseData, physicalResourceId=None, noEcho=False, reason=None):
+    responseUrl = event['ResponseURL']
+
+    responseBody = {
+        'Status': responseStatus,
+        'Reason': reason or "See the details in CloudWatch Log Stream: {}".format(context.log_stream_name),
+        'PhysicalResourceId': physicalResourceId or context.log_stream_name,
+        'StackId': event['StackId'],
+        'RequestId': event['RequestId'],
+        'LogicalResourceId': event['LogicalResourceId'],
+        'NoEcho': noEcho,
+        'Data': responseData
+    }
+
+    json_responseBody = json.dumps(responseBody)
+
+    print("Response body:")
+    print(json_responseBody)
+
+    headers = {
+        'content-type': '',
+        'content-length': str(len(json_responseBody))
+    }
+
+    try:
+        response = http.request(
+            'PUT', responseUrl, headers=headers, body=json_responseBody)
+        print("Status code:", response.status)
+        return responseBody
+
+    except Exception as e:
+
+        print("send(..) failed executing http.request(..):", e)
+        return {}
+
+
+def store_parameter(ssm, key, value):
+    ssm.put_parameter(Name="/hyperswitch/{}".format(key),
+                      Value=value, Overwrite=True, Type='String', Tier='Advanced')
+
+
+def lambda_handler(event, context):
+    try:
+        if event['RequestType'] == 'Create':
+            try:
+                worker()
+                message = "Completed Successfully"
+                status = "SUCCESS"
+            except Exception as e:
+                message = str(e)
+                status = "FAILED"
+
+            send(event, context, status,
+                 {
+                     "message": message
+                 })
+        elif event['RequestType'] == 'Delete':
+            keys = ["db_pass", "ca_cert", "tls_key", "tls_cert"]
+            ssm = boto3.client('ssm')
+            for key in keys:
+                parameter_name = "/keymanager/{}".format(key)
+                try:
+                    ssm.delete_parameter(Name=parameter_name)
+                except:
+                    print("Parameter {} doesn't exist.".format(parameter_name))
+
+            send(event, context, "SUCCESS", {"message": "No action required"})
+        else:
+            send(event, context, "SUCCESS", {"message": "No action required"})
+    except Exception as e:  # Use 'Exception as e' to properly catch and define the exception variable
+        send(event, context, "FAILED", {"message": str(e)})
+        return str(e)
+    # Return a success message
+    return '{ "status": 200, "message": "success" }'
--- lib/aws/keymanager/stack.ts
+++ lib/aws/keymanager/stack.ts
@@ -3,6 +3,7 @@ import { IVpc, InstanceType, SecurityGroup } from "aws-cdk-lib/aws-ec2";
 import { Vpc, SubnetNames } from "../networking";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
 
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 import * as eks from "aws-cdk-lib/aws-eks";
 import {
     AuroraPostgresEngineVersion,
@@ -11,6 +12,10 @@ import {
     DatabaseCluster,
     Credentials,
 } from "aws-cdk-lib/aws-rds";
+
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+
+import { readFileSync } from "fs";
 import { Secret } from "aws-cdk-lib/aws-secretsmanager";
 import { SubnetStack } from "../subnet";
 
@@ -24,6 +29,9 @@ export type KeymanagerConfig = {
     name: string;
     db_user: string;
     db_pass: string;
+    ca_cert: string;
+    tls_key: string;
+    tlk_cert: string;
 }
 
 export class Keymanager extends cdk.Stack {
@@ -67,6 +75,73 @@ export class Keymanager extends cdk.Stack {
             }
         });
 
+        const kms_policy_document = new iam.PolicyDocument({
+            statements: [
+                new iam.PolicyStatement({
+                    actions: ["kms:*"],
+                    resources: [kms_key.keyArn],
+                }),
+                new iam.PolicyStatement({
+                    actions: ["elasticloadbalancing:DeleteLoadBalancer",
+                        "elasticloadbalancing:DescribeLoadBalancers"],
+                    resources: ["*"],
+                }),
+                new iam.PolicyStatement({
+                    actions: ["ssm:*"],
+                    resources: ["*"],
+                }),
+                new iam.PolicyStatement({
+                    actions: ["secretsmanager:*"],
+                    resources: ["*"],
+                }),
+            ],
+        });
+        const lambda_role = new iam.Role(scope, "hyperswitch-lambda-role", {
+            assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+            inlinePolicies: {
+                "use-kms-sm": kms_policy_document,
+            },
+        });
+        const encryption_code = readFileSync(
+            "lib/aws/keymanager/encryption.py",
+        ).toString();
+
+        let secret = new Secret(scope, "keymanager-kms-userdata-secret", {
+            secretName: "KeymanagerKmsDataSecret",
+            description: "KMS encryptable secrets for Keymanager",
+            secretObjectValue: {
+                db_password: cdk.SecretValue.unsafePlainText(
+                    config.db_pass,
+                ),
+                kms_id: cdk.SecretValue.unsafePlainText(kms_key.keyId),
+                region: cdk.SecretValue.unsafePlainText(kms_key.stack.region),
+                ca_cert: cdk.SecretValue.unsafePlainText(config.ca_cert),
+                tls_key: cdk.SecretValue.unsafePlainText(config.tls_key),
+                tls_cert: cdk.SecretValue.unsafePlainText(config.tlk_cert),
+            },
+        });
+
+        const kms_encrypt_function = new Function(scope, "keymanager-kms-encrypt", {
+            functionName: "KeymanagerKmsEncryptionLambda",
+            runtime: Runtime.PYTHON_3_9,
+            handler: "index.lambda_handler",
+            code: Code.fromInline(encryption_code),
+            timeout: cdk.Duration.minutes(15),
+            role: lambda_role,
+            environment: {
+                SECRET_MANAGER_ARN: secret.secretArn,
+            },
+        });
+
+        const triggerKMSEncryption = new cdk.CustomResource(
+            scope,
+            "KeymanagerKmsEncryptionCR",
+            {
+                serviceToken: kms_encrypt_function.functionArn,
+            },
+        );
+
+        const kmsSecrets = new KmsSecrets(scope, triggerKMSEncryption);
         const keymanagerChart = cluster.addHelmChart("KeymanagerService", {
             chart: "hyperswitch-keymanager",
             repository: "https://juspay.github.io/hyperswitch-helm/v0.1.2",
@@ -79,6 +154,9 @@ export class Keymanager extends cdk.Stack {
                     secrets: {
                         iam_role: kms_role.roleArn,
                         region: process.env.CDK_DEFAULT_REGION,
+                        ca_cert: kmsSecrets.kms_encrypted_ca_cert,
+                        tls_key: kmsSecrets.kms_encrypted_tls_key,
+                        tls_cert: kmsSecrets.kms_encrypted_tls_cert,
                     }
                 },
                 postgresql: {
@@ -91,7 +169,7 @@ export class Keymanager extends cdk.Stack {
                             host: db.dbCluster?.clusterEndpoint.hostname,
                             port: 5432,
                             username: config.db_user,
-                            password: config.db_pass,
+                            password: kmsSecrets.kms_encrypted_db_pass,
                             database: "keymanager_db",
                         }
                     }
@@ -145,5 +223,22 @@ export class KeymanagerDB extends Construct {
         })
 
         this.dbCluster = dbCluster;
+    }
+}
+
+
+class KmsSecrets {
+    readonly kms_encrypted_tls_key: string;
+    readonly kms_encrypted_tls_cert: string;
+    readonly kms_encrypted_db_pass: string;
+    readonly kms_encrypted_ca_cert: string;
+
+    constructor(scope: Construct, kms: cdk.CustomResource) {
+
+        let message = kms.getAtt("message");
+        this.kms_encrypted_db_pass = ssm.StringParameter.valueForStringParameter(scope, "/keymanager/db_pass", 1);
+        this.kms_encrypted_tls_cert = ssm.StringParameter.valueForStringParameter(scope, "/keymanager/tls_cert", 1);
+        this.kms_encrypted_tls_key = ssm.StringParameter.valueForStringParameter(scope, "/keymanager/tls_key", 1);
+        this.kms_encrypted_ca_cert = ssm.StringParameter.valueForStringParameter(scope, "/keymanager/ca_cert", 1);
     }
 }
